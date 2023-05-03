760996d31374421da9a8c0cdb526fa4c5f323013
michaeldgarber <mdg711@gmail.com>
2023-05-03 12:57:45 -0600

    several updates to data processing and visualizations

--- scripts/link-zcta-hosp-data.R
+++ scripts/link-zcta-hosp-data.R
@@ -6,52 +6,26 @@ library(viridis)
 library(sf)
 library(mapview)
 
-# Link to measures (wide-form)-------
-hosp_both
-
 setwd(here("data-processed"))
 load("zcta_ca_geo.RData") #created heat-hosp-california/scripts/link-zcta-hosp-data.R
 load("zcta_ca_geo_simplified.RData")
-hosp_both_geo = hosp_both %>% 
+hosp_all_long_geo = hosp_all_long %>% 
   left_join(zcta_ca_geo_simplified, by = "zcta") %>% 
   st_as_sf()
 
-save(hosp_both_geo, file = "hosp_both_geo.RData")
-
-# Mapview tests-----
-# #ratio
-# hosp_both_geo %>% 
-#   filter(ratio_diff=="ratio") %>% 
-#   dplyr::select("zcta", "AI", "ratio_diff") %>% 
-#   mapview(
-#     col.regions= viridis_pal(option = "A", direction = 1),
-#     zcol = "AI",
-#     layer.name = "Tree canopy, Pop. based: Increase by 10%, all zip codes, Ratio")
-# 
-# #diff
-# hosp_both_geo %>% 
-#   filter(ratio_diff=="diff") %>% 
-#   mapview(
-#     col.regions= viridis_pal(option = "D", direction = 1),
-#     zcol = "AI",
-#           layer.name = "Tree canopy, Pop. based: Increase by 10%, all zip codes, Difference")
-
-
-#what are the unique values of the scenario abbreviations?
-names(hosp_both_geo)
 
 # Link to measure (long-form)-------
 #Linking to the simplified versions to keep file size smaller
 
-hosp_both_long_geo = hosp_both_long %>% 
+hosp_all_long_geo = hosp_all_long %>% 
   left_join(zcta_ca_geo_simplified, by = "zcta") %>% 
   st_as_sf()
 
-save(hosp_both_long_geo, file = "hosp_both_long_geo.RData")
+save(hosp_all_long_geo, file = "hosp_all_long_geo.RData")
 
-hosp_ratio_long_geo = hosp_ratio_long %>% 
+hosp_irr_long_geo = hosp_irr_long %>% 
   left_join(zcta_ca_geo_simplified, by = "zcta") %>% 
   st_as_sf()
-hosp_diff_long_geo = hosp_diff_long %>% 
+hosp_ird_long_geo = hosp_ird_long %>% 
   left_join(zcta_ca_geo_simplified, by = "zcta") %>% 
   st_as_sf()
--- scripts/read-ratios-diffs-maren.R
+++ scripts/read-ratios-diffs-maren.R
@@ -1,5 +1,5 @@
 #This script reads in Maren's data
-#Revised Mar 3, 2023
+#Revised May 2nd based on data that Maren sent me March 24th
 
 library(readr)
 library(readxl)
@@ -10,89 +10,231 @@ load("zcta_ca_list.RData")
 
 # Read data------
 zcta_ca_list
-setwd(here("data-input"))
+setwd(here("data-input","measures-most-recent"))
 #Maren revised filename Mar 3, 2023 from HospRatio.csv to StandardizedHosp.csv
-hosp_ratio = read_csv("StandardizedHosp.csv") %>% 
+#incidence rate ratio
+hosp_irr = read_csv("IRR_Mar24.csv") %>% 
   #Rename from ZIP to zcta for easy linking with other data
   rename(zcta = ZIP) %>% 
-  mutate(ratio_diff = "ratio")
+  mutate(measure = "irr")
 
-hosp_ratio
+hosp_irr
 
-hosp_diff = read_csv("AbsoluteBenefit.csv") %>% 
+#incidence rate difference
+hosp_ird = read_csv("IRD_Mar24.csv") %>% 
   rename(zcta = ZIP) %>% 
-  mutate(ratio_diff = "diff")
-  
-hosp_both = hosp_ratio %>% 
-  bind_rows(hosp_diff)
+  mutate(measure = "ird")
+hosp_ird
+
+#percent difference
+hosp_pd = read_csv("PctDif_Mar24.csv") %>% 
+  rename(zcta = ZIP) %>% 
+  mutate(measure = "pd")
+hosp_pd
+
+hosp_all = hosp_irr %>% 
+  bind_rows(hosp_ird) %>% 
+  bind_rows(hosp_pd)
 
 setwd(here("data-processed"))
-save(hosp_both, file = "hosp_both.RData")
+save(hosp_all, file = "hosp_all.RData")
 
 # Make data longer-----
 #To make it easier to write dynamic code for visualizations
-hosp_both_long = hosp_both %>% 
+hosp_all_long = hosp_all %>% 
   pivot_longer(
     cols = c(starts_with("A"), starts_with("B")),#key is using c()
     values_to = "value",
     names_to = "scenario") %>% 
-  as_tibble() 
+  as_tibble() %>% 
+  ## add some additional scenario vars-------
+  mutate(
+    scenario_intervention=case_when(
+      #needle, haystack
+      grepl("A",scenario)~"Tree", #Tree canopy
+      TRUE~"Imp" #Impervious surface cover
+    ),
+    #one of 3 categories
+    scenario_type_3=case_when(
+      grepl("III",scenario)~"Prop. Univ.", 
+      grepl("II",scenario)~"Targeted", 
+      grepl("I",scenario)~"Population-based"
+  ),
+  
+  #
+  scenario_sub_type=case_when(
+    #Note these are excluding the pop.based ones
+    grepl("1",scenario)~"Exposure", 
+    grepl("2",scenario)~"SVI", 
+    grepl("3",scenario)~"Hosp. burden"
+  ),
+  #a 1-7 number within the intervention type
+  scenario_type_7=case_when(
+    grepl("III1",scenario)~"Prop. Univ. 1",
+    grepl("III2",scenario)~"Prop. Univ. 2", 
+    grepl("III3",scenario)~"Prop. Univ. 3", 
+    grepl("II1",scenario)~"Targeted 1",
+    grepl("II2",scenario)~"Targeted 2",
+    grepl("II3",scenario)~"Targeted 3",
+    grepl("I",scenario)~"Pop. based"
+  ),
+  #abbreviated version
+  scenario_type_7_abbrev=case_when(
+    grepl("III1",scenario)~"PU 1",
+    grepl("III2",scenario)~"PU 2", 
+    grepl("III3",scenario)~"PU 3", 
+    grepl("II1",scenario)~"T 1",
+    grepl("II2",scenario)~"T 2",
+    grepl("II3",scenario)~"T 3",
+    grepl("I",scenario)~"PB"
+  ),
+  
+  
+  )
+
+hosp_all_long
+table(hosp_all_long$scenario)
+table(hosp_all_long$scenario_intervention)
+table(hosp_all_long$scenario_type_3)
+table(hosp_all_long$scenario_type_7)
+table(hosp_all_long$scenario_sub_type)
 
 setwd(here("data-processed"))
-save(hosp_both_long, file = "hosp_both_long.RData")
+save(hosp_all_long, file = "hosp_all_long.RData")
+table(hosp_all_long$scenario)
+#Examine distribution of each-----
+table(hosp_all_long$scenario)
+hosp_all_long %>% 
+  filter(measure=="irr") %>% 
+  ggplot(aes(value))+
+  geom_histogram()+
+  facet_grid(
+    rows=vars(scenario_type_3),
+    cols=vars(scenario_intervention)
+  )
 
-hosp_both_long %>% 
-  filter(ratio_diff=="ratio") %>% 
+hosp_all_long %>% 
+  filter(measure=="irr") %>% 
+  ggplot(aes(value))+
+  geom_histogram()+
+  facet_grid(
+    rows=vars(scenario_type_7),
+    cols=vars(scenario_intervention)
+  )
+
+
+hosp_all_long %>% 
+  filter(measure=="irr") %>% 
+  dplyr::select(value) %>% 
   summary()
 
-hosp_both_long %>% 
-  filter(ratio_diff=="diff") %>% 
+
+
+hosp_all_long %>% 
+  filter(measure=="ird") %>% 
+  ggplot(aes(value))+
+  geom_histogram()
+
+hosp_all_long %>% 
+  filter(measure=="ird") %>% 
+  filter(value<0.003) %>% #avoid outliers for easier vis
+  ggplot(aes(value))+
+  geom_histogram()+
+  facet_grid(
+    rows=vars(scenario_type_3),
+    cols=vars(scenario_intervention)
+  )
+
+
+hosp_all_long %>% 
+  filter(measure=="ird") %>% 
+  filter(value<0.003) %>% #avoid outliers for easier vis
+  ggplot(aes(value))+
+  geom_histogram()+
+  facet_grid(
+    rows=vars(scenario_type_7),
+    cols=vars(scenario_intervention)
+  )
+
+hosp_all_long %>% 
+  filter(measure=="ird") %>% 
+  dplyr::select(value) %>% 
   summary()
 
-# Create categories for ratios and differences--------
+hosp_all_long %>% 
+  filter(measure=="pd") %>% 
+  ggplot(aes(value))+
+  geom_histogram()
+
+
+hosp_all_long %>% 
+  filter(measure=="pd") %>% 
+  dplyr::select(value) %>% 
+  summary()
+
+
+# Create categories for each measure--------
 #Limit to about 5 bins
-.65/3
-hosp_ratio_long = hosp_both_long %>% 
-  filter(ratio_diff=="ratio") %>% 
+## irr-------
+hosp_irr_long = hosp_all_long %>% 
+  filter(measure=="irr") %>% 
   mutate(
+    #see histogram for values
     value_cat = 
       cut(
           value,
           include.lowest=TRUE,
-          breaks=c(
-            min(value,na.rm=TRUE),
-            quantile(value, probs =c(0.35), na.rm=TRUE),
-            quantile(value, probs =c(0.55), na.rm=TRUE),
-            quantile(value, probs =c(0.75), na.rm=TRUE),
-            quantile(value, probs =c(0.9), na.rm=TRUE),
-            max(value,na.rm=TRUE)
-          )),
+          breaks=c(0, 0.66, .9, .95, 0.975, 1)
+          ),
     #mapview doesn't like factors, so try
     value_cat_char = as.character(value_cat)
   )
 
-  
-table(hosp_ratio_long$value_cat)
 
-hosp_diff_long = hosp_both_long %>% 
-  filter(ratio_diff=="diff") %>% 
+table(hosp_irr_long$value_cat)
+length(table(hosp_irr_long$value_cat))
+
+## ird------
+hosp_ird_long = hosp_all_long %>% 
+  filter(measure=="ird") %>% 
   mutate(
     value_cat = 
       cut(
-        #see below distribution
+        #see histograms above 
         value,
         include.lowest=TRUE,
         breaks=c(
-          min(value,na.rm=TRUE),
-          #more cut-offs at the top of the dist'n because that's where there's more variation
-          quantile(value, probs =c(0.43), na.rm=TRUE),
-          quantile(value, probs =c(0.6), na.rm=TRUE),
-          quantile(value, probs =c(0.8), na.rm=TRUE),
-          quantile(value, probs =c(0.9), na.rm=TRUE),
+          0, 
+          0.00001,
+          0.0001,
+#          0.0005,
+          0.001,
+          0.003,
           max(value,na.rm=TRUE)
         )),
     #mapview doesn't like factors, so try
     value_cat_char = as.character(value_cat)
   )
 
-table(hosp_diff_long$value_cat)
+table(hosp_ird_long$value_cat)
+length(table(hosp_ird_long$value_cat))
+
+## pd--------
+hosp_pd_long = hosp_all_long %>% 
+  filter(measure=="pd") %>% 
+  mutate(
+    value_cat = 
+      cut(
+        #see histograms above 
+        value,
+        include.lowest=TRUE,
+        breaks=c(
+          min(value,na.rm=TRUE),
+          1,5,10,20,
+          max(value,na.rm=TRUE)
+        )),
+    value_cat_char = as.character(value_cat)
+  )
+
+table(hosp_pd_long$value_cat)
+length(table(hosp_pd_long$value_cat))
